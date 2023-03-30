511bad3d8fa23c68e67a1ecbde7b50f19b21b40c
CRUNZEX <69286857+CRUNZEX@users.noreply.github.com>
2023-03-30 18:00:57 +0700

    Add Cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources = { r'/*': { 'origins': '*' } } )
+
 
 # Page: HTTP error handling
 # @app.errorhandler(404)
